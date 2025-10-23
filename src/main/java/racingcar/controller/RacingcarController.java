40389b8b852a4ef3c79b1b2c55664d10b34c1627
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-10-23 23:44:59 +0900

    feat: 자동차 이름 받기 기능 추가

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,11 +1,14 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingcarController {
     private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
 
     public void run() {
         outputView.printRacingcarNameInputGuide();
+        String userInput = inputView.getUserInput();
     }
 }
