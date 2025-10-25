b4a737a6db55afd270cfd24263fd49bf641c0993
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-10-25 19:33:02 +0900

    refactor: 상수명 INPUT에서 ENTER로 변경
    OutputView에서 INPUT의 네이밍이 혼란성 야기
    입력의 다른 뜻인 ENTER로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,14 +1,14 @@
 package racingcar.view;
 
 public class OutputView {
-    private static final String RACINGCAR_NAME_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String TOTAL_ROUND_INPUT_GUIDE = "시도할 횟수는 몇 회인가요?";
+    private static final String RACINGCAR_NAME_ENTER_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TOTAL_ROUND_ENTER_GUIDE = "시도할 횟수는 몇 회인가요?";
 
     public void printRacingcarNameInputGuide() {
-        System.out.println(RACINGCAR_NAME_INPUT_GUIDE);
+        System.out.println(RACINGCAR_NAME_ENTER_GUIDE);
     }
 
     public void printTotalRoundInputGuide() {
-        System.out.println(TOTAL_ROUND_INPUT_GUIDE);
+        System.out.println(TOTAL_ROUND_ENTER_GUIDE);
     }
 }
