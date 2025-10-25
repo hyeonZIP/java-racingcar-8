97ab3061e3f8a86b4ff244a358a506c4a92a0762
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-10-25 22:30:57 +0900

    feat: 자동차 이동 조건 생성을 위한 구현체 추가
    0~9 사이의 정수 값에서 랜덤 값을 반환하고 4 이상일 경우 전진

---
+++ src/main/java/racingcar/domain/RandomMoveStrategy.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomMoveStrategy implements MoveStrategy {
+    private static final int MINIMUM_RANDOM_RANGE = 0;
+    private static final int MAXIMUM_RANDOM_RANGE = 9;
+    private static final int MOVE_THRESHOLD = 4;
+
+    @Override
+    public boolean canMove() {
+        return Randoms.pickNumberInRange(MINIMUM_RANDOM_RANGE, MAXIMUM_RANDOM_RANGE) >= MOVE_THRESHOLD;
+    }
+}
